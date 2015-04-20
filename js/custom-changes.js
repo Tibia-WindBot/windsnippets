91431c8a72d6914f712d36dbeea0ab03a73d00f9
Leonardo Falk <leonardo.falk@hotmail.com>
2015-04-20 16:17:15 -0300

    Added selector guides
    Added themes and UI selector
    Added a test page
    Added cookies and clipboard plugins
    
    Changed logo size and alignment

--- js/custom-changes.js
+++ js/custom-changes.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
-	$(".button-collapse").sideNav();
+	$(".button-collapse").sideNav({
+		menuWidth: 250,
+		edge: "left",
+		closeOnClick: true
+	});
+
 	$(".dropdown-button").dropdown();
+
+	$(".tooltipped").tooltip({delay: 50});
+
+	$(".modal-trigger").leanModal({
+		dismissible: true,
+		opacity: .5,
+		in_duration: 200,
+		out_duration: 200,
+	});
 });
