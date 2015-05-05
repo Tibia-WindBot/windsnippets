e18f0fc0e7acf08a472ad8e63d5c0638dcb3dac7
Leonardo Falk <leonardo.falk@hotmail.com>
2015-05-05 09:11:35 -0300

    Added theme changer
    Added theme+background changer
    Added mode changer
    Removed Test Page
    Commented stuff that will not be available in the first versions

--- js/custom-changes.js
+++ js/custom-changes.js
@@ -5,14 +5,38 @@ $(document).ready(function(){
 		closeOnClick: true
 	});
 
-	$(".dropdown-button").dropdown();
+	$(".dropdown-button").dropdown({
+		inDuration: 1000,
+		outDuration: 250,
+	});
 
 	$(".tooltipped").tooltip({delay: 50});
 
 	$(".modal-trigger").leanModal({
 		dismissible: true,
 		opacity: .5,
-		in_duration: 200,
-		out_duration: 200,
+		in_duration: 500,
+		out_duration: 250,
 	});
+
+	$('select').material_select();
+    
+    var theme = "ace/theme/ambiance";
+    var mode = "ace/mode/lua";
+	var editor = ace.edit("editor");
+    
+	editor.setTheme(theme);
+	editor.getSession().setMode(mode);
+	editor.setPrintMarginColumn(-1);
+
+	$('#editor-mode').on('change', function() {
+        editor.getSession().setMode('ace/mode/' + $(this).val().toLowerCase());
+	});
+
+    $('#editor-theme').on('change', function() {
+        editor.setTheme('ace/theme/' + $(this).val().toLowerCase());
+        $("body").css("background-color", $('#editor').css("background-color"));
+        // ToDo: fix background color (black hides shadow and other effects)
+    });
 });
+
